f81f6d3e1a0df2a43a8a3e7a9d8cd78b594e3a26
VusalGuliyevv <vsalquliyev@gmail.com>
2024-08-14 02:54:50 +0400

    created User DTO

---
+++ src/main/java/com/company/loginform/dto/UserDto.java
@@ -0,0 +1,45 @@
+package com.company.loginform.dto;
+
+public class UserDto {
+
+    private int id;
+
+    private String name;
+
+    private String password;
+
+    public UserDto() {
+    }
+
+    public UserDto(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return name;
+    }
+
+    public void setUsername(String username) {
+        this.name = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
+
